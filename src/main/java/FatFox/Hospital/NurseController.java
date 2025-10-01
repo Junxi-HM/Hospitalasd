42caece6df05f854eb4276fa84c7852a553158da
kim-1111 <em.huang.yixin@gmail.com>
2025-10-01 16:33:17 +0200

    test login

--- src/main/java/FatFox/Hospital/NurseController.java
+++ src/main/java/FatFox/Hospital/NurseController.java
@@ -3,27 +3,39 @@ package FatFox.Hospital;
 import FatFox.Hospital.Nurse;
 import FatFox.Hospital.NurseService;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class NurseController {
-    @Autowired
-    private NurseService nurseService;
+public class NurseController implements WebMvcConfigurer {
+	@Autowired
+	private NurseService nurseService;
 
-    @GetMapping("/nurses/search")
-    public List<Nurse> searchNurses(@RequestParam String name) {
-        return nurseService.searchByName(name);
-    }
-    
-    @GetMapping("/nurses/index")
+	@GetMapping("/nurses/search")
+	public List<Nurse> searchNurses(@RequestParam String name) {
+		return nurseService.searchByName(name);
+	}
+
+	@GetMapping("/nurses/index")
 	public List<Nurse> getAll() {
 		return nurseService.getAllNurses();
 	}
+
+	@Configuration
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+	}
+
 }
